11265c95fefcabbed0af229043f08ace6965869c
unknown <pal.attila2000@gmail.com>
2024-05-23 19:03:25 +0300

    Fixed 16to8

--- src/flow_16to8.v
+++ src/flow_16to8.v
@@ -22,36 +22,32 @@ output reg              dst_val  ,  // valid, active high
 input                   dst_rdy  ,  // ready, active high
 output reg [8-1:0]      dst_data    // data, steady on valid
 );
-
-reg [1:0] out_cnt;
-//1 - dst_data = input[7:0]
-//2 - dst_data = input[15:8]
-
+reg cnt;
+wire output_accepted;
 wire inp_val;
-assign inp_val = src_val & src_rdy;
+
+assign inp_val = src_val & src_rdy & cfg_en;
+assign output_accepted = dst_val & dst_rdy ;
 
 always @(posedge clk or negedge rst_n)
-if (~rst_n)            out_cnt <= 0          ; else
-if (~cfg_en)           out_cnt <= 0          ; else
-if (out_cnt == 2)      out_cnt <= 0          ; else
-if (dst_val & dst_rdy) out_cnt <= out_cnt + 1;
+if (~rst_n)             cnt <= 0   ; else
+if (~cfg_en)            cnt <= 0   ; else
+if (output_accepted)    cnt <= ~cnt;
 
 always @(posedge clk or negedge rst_n)
-if (~rst_n)       dst_data <= 0             ; else
-if (~cfg_en)      dst_data <= 0             ; else
-if (out_cnt == 1) dst_data <= src_data[15:8] ; else
-if (out_cnt == 2) dst_data <= src_data[7:0];
+if (~rst_n)       dst_data <= 0                                   ; else
+if (~cfg_en)      dst_data <= 0                                   ; else
+                  dst_data <= cnt ? src_data[15:8] : src_data[7:0];
 
 always @(posedge clk or negedge rst_n)
-if (~rst_n)                  src_rdy <= 0; else
-if (~cfg_en)                 src_rdy <= 0; else
-if (~dst_val & out_cnt == 0) src_rdy <= 1; else
-                             src_rdy <= 0; 
+if (~rst_n)                 src_rdy <= 1; else
+if (inp_val)                src_rdy <= 0; else
+if (cnt & output_accepted)  src_rdy <= 1; 
 
 always @(posedge clk or negedge rst_n)
-if (~rst_n)       dst_val <= 0; else
-if (~cfg_en)      dst_val <= 0; else
-if (inp_val)      dst_val <= 1; else
-if (out_cnt == 2) dst_val <= 0;
+if (~rst_n)                dst_val <= 0; else
+if (~cfg_en)               dst_val <= 0; else
+if (inp_val)               dst_val <= 1; else
+if (cnt & output_accepted) dst_val <= 0;
                                  
 endmodule // flow_16to8
