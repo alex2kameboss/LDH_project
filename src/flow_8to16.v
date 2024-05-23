309651f1aab3634ba5841284b731e04143b01533
Alexandru Puscasu <palexandru2000@gmail.com>
2024-05-23 17:30:25 +0300

    first version of 8 to 16

--- src/flow_8to16.v
+++ src/flow_8to16.v
@@ -23,6 +23,28 @@ input                   dst_rdy  ,  // ready, active high
 output reg [16-1:0]     dst_data    // data, steady on valid
 );
 
+reg counter;
+
+wire valid_input;
+assign valid_input = ~dst_val & (src_rdy & src_val) & cfg_en;
+
+always@(posedge clk)
+    if(valid_input)     dst_data<={src_data,dst_data[15:8]};
+
 // place your code here
+
+always @(posedge clk or negedge rst_n)
+if(~rst_n)                  src_rdy <= 1'b1;    else
+if(dst_val & dst_rdy)       src_rdy <= 1'b1;    else
+if(counter & valid_input)   src_rdy <= 1'b0;      
+
+always @(posedge clk or negedge rst_n)
+    if (~rst_n)         counter <= 'd0;             else
+    if (valid_input)    counter <= ~counter;     
+
+always @(posedge clk or negedge rst_n)
+    if (~rst_n)                 dst_val <= 1'b0;            else
+    if (counter & valid_input)  dst_val <= 1'b1;            else
+    if (dst_val & dst_rdy)      dst_val <= 1'b0;
 
 endmodule // flow_8to16
