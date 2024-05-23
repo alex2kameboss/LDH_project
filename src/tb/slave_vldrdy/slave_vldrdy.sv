f84602ce26308dafe4b150b5442eafc9217fbbcd
Shareby <vali.floricescu@gmail.com>
2024-05-23 18:15:37 +0300

    initial version slave_vldrdy

--- src/tb/slave_vldrdy/slave_vldrdy.sv
+++ src/tb/slave_vldrdy/slave_vldrdy.sv
@@ -1,1 +1,36 @@
+module slave_vldrdy #(
+    parameter DWIDTH = 8
+)
+(
+    input                 clk      ,  // synchronous active on rising edge
+    input                 rst_n    ,  // asynchronous, active low
+    input                 cfg_en   ,  // enable, active high
+                                        // protocol can be violated on disable
+    // 16 bit output flow     
+    input                 dst_val  ,  // valid, active high
+    output                dst_rdy  ,  // ready, active high
+    input  [DWIDTH-1:0]   dst_data    // data, steady on valid
+);
 
+int write_counter = 0;
+
+integer fd;
+
+initial begin
+    if(DWIDTH == 8)
+        fd = $fopen("file_output8b.raw","w" );
+    else
+        fd = $fopen("file_output16b.raw","w");
+end
+
+always @(posedge clk)
+if (cfg_en && dst_val && dst_rdy)
+begin
+    write_counter++;
+    if(DWIDTH == 'd8)
+        $fwrite(fd,"%c",dst_data  );
+    else
+        $fwrite(fd,"%c%c",dst_data);
+end
+
+endmodule
